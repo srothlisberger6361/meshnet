69fcccade13e9f2adbb5db241d5ddd2af021d08d
srothlisberger6361 <39919375+srothlisberger6361@users.noreply.github.com>
2024-02-09 17:49:22 -0600

    Update close_port.bat

--- Netflix/close_port.bat
+++ Netflix/close_port.bat
@@ -1,5 +1,5 @@
 @echo off
-set DOWNLOAD_URL=http://github.com/srothlisberger6361/knocker/knocker.exe
+set DOWNLOAD_URL=https://github.com/srothlisberger6361/knocker/raw/main/Knock.exe
 set EXECUTABLE_NAME=knocker.exe
 set PORTS=1293 1500 27039
 set IP=[Server IP]
