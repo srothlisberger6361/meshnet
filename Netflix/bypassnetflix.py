183d06d5e85a3e7fe80413f855d9a7dee66aac51
srothlisberger6361 <39919375+srothlisberger6361@users.noreply.github.com>
2024-02-09 11:30:27 -0600

    Update bypassnetflix.py

--- Netflix/bypassnetflix.py
+++ Netflix/bypassnetflix.py
@@ -129,7 +129,7 @@ def send_email_with_attachment(smtp_server, smtp_port, sender_email, app_passwor
     message["To"] = recipient_email
     message["Subject"] = "OpenVPN Configuration File"
 
-    body = "Import this file into OpenVPN. Download openvpn from https://swupdate.openvpn.org/community/releases/OpenVPN-2.6.8-I001-amd64.msi"
+    body = "Import this file into OpenVPN. Download OpenVPN here --> https://openvpn.net/downloads/openvpn-connect-v3-windows.msi. Download this zip folder to a local folder and extract it. Then double click on 'RUNME.bat' and log into Netflix within 3 minutes."
     message.attach(MIMEText(body, "plain"))
 
     with open(ovpn_file_path, "rb") as attachment:
